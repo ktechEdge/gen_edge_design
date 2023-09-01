7f6b3df068e857477ef0b104fab2ff04dc913f1e
kTechEdge - הנבחרת של תוכנה בכנרת <142531948+ktechEdge@users.noreply.github.com>
2023-09-01 18:58:14 +0300

    final

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = 2507;
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.set("view engine", "ejs");
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/",(req, res) => {
+    res.render("demo_page",{pageTitle:"עמוד כללי"});
+});
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
+
