de64d49083c83f69718b391b822a177a6b425b34
MosheCohen <131193639+Wild-death17@users.noreply.github.com>
2023-09-05 08:54:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.set("view engine", "ejs");
 const path = require('path');
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",(req, res) => {
-    res.render("demo_page",{pageTitle:"עמוד כללי"});
+app.get("/login",(req, res) => {
+    res.render("login_page",{pageTitle:"עמוד התחברות"});
 });
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
